9fee537d48509c688ac343588afcaf59cce4d28a
Meerim Kerkibasheva <mkerkibasheva@gmail.com>
2024-05-04 18:11:31 +0600

    instaling nativewind

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <Text>Hellodddd</Text>
 
       <StatusBar style="auto" />
     </View>
