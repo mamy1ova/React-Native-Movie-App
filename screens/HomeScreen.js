ef9b7d60090c976a698ec676f4dc064d7c773d33
Meerim Kerkibasheva <mkerkibasheva@gmail.com>
2024-05-23 22:55:02 +0600

    commit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,11 @@
+import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View className="mb-8">
       <Text className="text-dark mb-3">HomeScreen</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
